b2cc373aa9286dbbf6cdee88f650c56e4f38e01d
Tyler Bertrand <tbertrand@gradle.com>
2025-02-19 16:53:12 -0600

    Apply same caching configuration in included build and top-level build

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.gradle.develocity") version ("3.19.2")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version ("2.1")
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+}
+
+buildCache {
+    remote(develocity.buildCache) { }
+}
